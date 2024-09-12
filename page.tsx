e7fc08263f238227446ed32a3da294c507d06754
CloudyZeeshan <taimoorhassankhan2@gmail.com>
2024-09-12 11:56:51 -0700

    he will link in project01

---
+++ page.tsx
@@ -0,0 +1,18 @@
+function MyName(){
+  return(
+    <div>
+    <h1><i><b>Zeeshan Haider</b></i></h1>
+    <h1><i><b>Taimoor Hassan</b></i></h1>
+    </div>
+  )
+}
+
+export default function Home(){
+  return(
+    <div>Thursday class
+      <MyName/>
+      <MyName/>
+      <MyName/>
+    </div>
+  )
+}
